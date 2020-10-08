5b49873fa0ff8016e936382a6193c32373e16cef
origogi <ahn192016@naver.com>
2020-10-08 16:53:58 +0900

    Add greeting

---
+++ greeting.js
@@ -0,0 +1,39 @@
+const form = document.querySelector(".js-userform"),
+  input = form.querySelector("input"),
+  greeting = document.querySelector(".js-greeting");
+
+function askForName() {
+  form.classList.add("show");
+  form.addEventListener("submit", handleSubmit);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const currentValue = input.value;
+  saveName(currentValue);
+  showGreeting(currentValue);
+}
+
+function saveName(text) {
+  localStorage.setItem("name", text);
+}
+
+function showGreeting(text) {
+  form.classList.remove("show");
+  greeting.classList.add("show");
+  greeting.innerText = `Hello ${text}`;
+}
+
+function loadName() {
+  const userName = localStorage.getItem("name");
+  if (userName === null) {
+    askForName();
+  } else {
+    showGreeting(userName);
+  }
+}
+
+function init() {
+  loadName();
+}
+init();
