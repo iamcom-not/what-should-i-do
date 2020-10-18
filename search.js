ebc0c15909cfce7e8a7241674d5508808a50a846
origogi <ahn192016@naver.com>
2020-10-19 06:00:49 +0900

    Add google, naver, youtube

--- search.js
+++ search.js
@@ -1,16 +1,36 @@
-const searchForm = document.querySelector(".js-searchForm"),
-searchInput = searchForm.querySelector("input"),
+const searchForm = document.querySelector(".searchForm"), 
+searchGoogle = document.querySelector(".js-searchGoogle"),
+searchNaver = document.querySelector(".js-searchNaver"),
+searchYoutube = document.querySelector(".js-searchYoutube"),
+googleInput = searchGoogle.querySelector("input"),
+naverInput = searchNaver.querySelector("input"),
+youtubeInput = searchYoutube.querySelector("input"),
 searchLink =searchForm.querySelector("a");
 
 
 function handleSearch(e){
     e.preventDefault();
-    console.log("here");
-    const currentValue = searchInput.value;
-    //~~~~함수
-    window.open(`https://www.google.com/search?q=${currentValue}&oq=${currentValue}&aqs=chrome..69i57.2440j0j1&sourceid=chrome&ie=UTF-8`);
-}
+    const which = e.target.classList;
+    if(which[0]==='js-searchGoogle'){
+        const currentValue =googleInput.value;
+        window.open(`https://www.google.com/search?q=${currentValue}&oq=${currentValue}&aqs=chrome..69i57.1030j0j1&sourceid=chrome&ie=UTF-8`);
+        googleInput.value= "";
+    }else if(which[0]==='js-searchNaver'){
+        const currentValue = naverInput.value;
+        window.open(`https://search.naver.com/search.naver?sm=top_hty&fbm=1&ie=utf8&query=${currentValue}`);
+        naverInput.value= "";
+    }else{
+        const currentValue = youtubeInput.value;
+        window.open(`https://www.youtube.com/results?search_query=${currentValue}`);
+        youtubeInput.value= "";
+    }}
+        
+        
+
+
 function init(){
-    searchForm.addEventListener("submit", handleSearch);
+    searchGoogle.addEventListener("submit", handleSearch);
+    searchNaver.addEventListener("submit", handleSearch);
+    searchYoutube.addEventListener("submit", handleSearch);
 }
 init();
